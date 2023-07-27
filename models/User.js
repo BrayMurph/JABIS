1b1f1896db3542d767b28dcd993a22a20556e34d
Asiak <asiakemp24@gmail.com>
2023-07-27 18:43:53 -0400

    created the verify password function in the User model

--- models/User.js
+++ models/User.js
@@ -8,12 +8,14 @@ const sequelize = require('../config/connection');
 class User extends Model {
     // add methods/associations/etc...
 }
-
+// attaches the local authentication strategy from passport.js to the user model
+// automatically adds username and password to the model
 passportLocalSequelize.attachToUser(User, {
     usernameField: 'username',
     hashField: 'password',
 });
-
+// used to find a user in the database based on username. Uses findOne method to retrieve the matching username
+// returns null if no user is found
 User.findByUsername = async function (username, callback) {
     try {
         const user = await this.findOne({ where: {username}});
@@ -21,6 +23,16 @@ User.findByUsername = async function (username, callback) {
     } catch (err) {
         callback(err);
     }
-}
+};
 
+// method to verify a password against the hashed password
+// verifyPasswordHash method provided by passport-local-sequelize
+User.prototype.verifyPassword = function (password, callback) {
+    this.verifyPasswordHash(password, this.password, function (err, isMatch) {
+        if (err) return callback(err);
+        callback(null, isMatch);
+    });
+};
+
+// exports the User model
 module.exports = User
