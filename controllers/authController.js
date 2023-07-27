a07ba2aabc72661f947e5a62b329dac6d6087b41
Asiak <asiakemp24@gmail.com>
2023-07-26 21:14:08 -0400

    created the authController.js configured passportjs, authentication logic, serialize and deserialize, exports login and logout and ensureAuth, also added middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,76 @@
-// const passport = require('passport');
-// const LocalStrategy = require('passport-local').Strategy;
-// const User = require('../models/User');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/User');
 
-// // configure passport
+// configure passport
+passport.use(new LocalStrategy(
+    // lets us customize the error
+    { failWithError: true},
+    function (username, password, done) {
+        // logic for authentication. Making sure the username and password is correct.
+        User.findByUsername(username, function(err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username or password'});
+            }
+            user.verifyPassword(password, function (err, isMatch) {
+                if (err) {
+                    return done(err);
+                }
+                if (!isMatch) {
+                    return done(null, false, { message: 'Incorrect username or password'});
+                }
+                return done(null, user);
+            });
+        });
+    }
+));
+
+// serialize and deserialize the User
+// serialize stores the user ID in the session
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+// deserialize retrieves user info based on the user ID stored in the session
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+// export the login and logout functions as well as the middle ware
+// makes sure routes are protected with the ensureAuthenticated action
+// may reorganize?
+module.exports = {
+    login: function(req, res, next) {
+        // middleware
+        passport.authenticate('local', function(err, user, info) {
+            if (err) {
+                return next(err);
+            }
+            if (!user) {
+                return res.status(401).json(info)
+            }
+            req.login(user, function(err) {
+                if (err) {
+                    return next(err);
+                }
+                return res.json({ message: 'Login successful', user});
+            });
+        })(req, res, next);
+    },
+
+    logout: function(req, res) {
+        req.logout();
+        res.json({ message: 'Logout successful'});
+    },
+
+    ensureAuthenticated: function(req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        res.status(401).json({ message: 'Authentication required'});
+    }
+};
--- models/User.js
+++ models/User.js
@@ -1,1 +1,2 @@
 // Define the user model add in needed passport functions
+// add findByUsername function and verifyPassword function for passportjs
