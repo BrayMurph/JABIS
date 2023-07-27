93310d0c0538a7267d37d3d6061a0ae0ac4b8914
Asiak <asiakemp24@gmail.com>
2023-07-26 21:25:54 -0400

    created the findByUsername function

--- models/User.js
+++ models/User.js
@@ -1,2 +1,26 @@
 // Define the user model add in needed passport functions
 // add findByUsername function and verifyPassword function for passportjs
+
+const { Model, DataTypes } = require('sequelize');
+const passportLocalSequelize = require('passport-local-sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    // add methods/associations/etc...
+}
+
+passportLocalSequelize.attachToUser(User, {
+    usernameField: 'username',
+    hashField: 'password',
+});
+
+User.findByUsername = async function (username, callback) {
+    try {
+        const user = await this.findOne({ where: {username}});
+        callback(null, user || null);
+    } catch (err) {
+        callback(err);
+    }
+}
+
+module.exports = User
