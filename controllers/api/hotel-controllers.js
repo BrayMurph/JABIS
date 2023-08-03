16206256e8bf93f627ad7d91042e5ae48fd25ccf
BrayMurph <brayxmurph@gmail.com>
2023-08-03 13:26:04 -0400

    Made the data pulls through the api

--- controllers/api/hotel-controllers.js
+++ controllers/api/hotel-controllers.js
@@ -1,4 +1,5 @@
-const { ensureAuthenticated, fetchHotelData } = require('./authController');
+const { ensureAuthenticated, fetchHotelData } = require('../authController');
+const router = require('express').Router();
 
 router.get('/api/Hotels', async (req, res) => {
   try {
