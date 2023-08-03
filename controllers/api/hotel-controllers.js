51daed33f8eaad3ed3e58ac2a84f2ee982c663ae
Ijordan4 <124106922+Ijordan4@users.noreply.github.com>
2023-08-03 11:35:24 -0400

    Update hotel-controllers.js

--- controllers/api/hotel-controllers.js
+++ controllers/api/hotel-controllers.js
@@ -1,14 +1,13 @@
-const { ensureAuthenticated, fetchHotelData } = require('./authController');
+const express = require('express');
+const router = express.Router();
 
-router.get('/api/Hotels', async (req, res) => {
+router.get('/api', (req, res) => {
   try {
-    const hotelData = await fetchHotelData();
-    
-    res.json(hotelData);
+
+    res.json({ message: 'This is a sample response' });
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
 
 module.exports = router;
