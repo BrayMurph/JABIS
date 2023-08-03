ad7d719d6c3dd84eb30d69ed8558def45b7d7c31
TheWiseSailor <rudasilljay@gmail.com>
2023-08-03 14:46:42 -0400

    this was here when ever I git pulled from  main

---
+++ controllers/signupController.js
@@ -0,0 +1,26 @@
+// signupController.js
+
+const User = require('../models/User');
+
+exports.signup = async (req, res) => {
+  try {
+    const { first_name, last_name, email, username, password } = req.body;
+    console.log('Password before hash:', password);
+
+    // Add any additional validation or data processing here
+
+    const newUser = await User.create({
+      first_name,
+      last_name,
+      email,
+      username,
+      password,
+    });
+
+    // Redirect to the login page after successful signup
+    res.redirect('/login');
+  } catch (err) {
+    console.error('Error saving new user:', err);
+    // Handle the error appropriately (e.g., show an error message)
+  }
+};
