615832ffa4a585b5bb4e1e2478b55f005937f445
Asiak <asiakemp24@gmail.com>
2023-08-05 12:28:04 -0400

    updated login.js to try to handle logout too

--- public/js/login.js
+++ public/js/login.js
@@ -1,63 +1,41 @@
-// Call the updateLoginLogoutButton function when the page loads
-document.addEventListener('DOMContentLoaded', async () => {
-    try {
-      // Send a GET request to check if the user is logged in
-      const response = await fetch('/api/users/checkLoggedIn');
-      const data = await response.json();
-  
-      // Update the login/logout button based on the authentication status
-      updateLoginLogoutButton(data.isLoggedIn);
-    } catch (err) {
-      console.error('Error:', err);
-      alert('An error occurred. Please try again later.');
-    }
-  });
-  
-  // target the form and the event listener is submit
-  const loginForm = document.querySelector('form');
-  
-  // create event listener that listens to a submit
-  loginForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
-  
-    // capture inputs
-    const formData = new FormData(loginForm);
-    const username = formData.get('username');
-    const password = formData.get('password');
-  
-    try {
-      // handler function should send post route
-      // miniproject const response method post
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-      if (response.ok) {
-        // if response is ok, update the login/logout button and redirect to the homepage
-        updateLoginLogoutButton(true);
-        window.location.href = '/';
-      } else {
-        alert(response.statusText);
-      }
-    } catch (err) {
-      console.error('Error:', err);
-      alert('An error occurred. Please try again later.');
-    }
-  });
-  
-  // function to update the login/logout button
-  const updateLoginLogoutButton = (isLoggedIn) => {
+// function to update the login/logout button
+const updateLoginLogoutButton = (isLoggedIn) => {
     const loginLogoutContainer = document.getElementById('loginlogout');
+    const logoutLink = loginLogoutContainer.querySelector('a[href="/logout"]');
+    
     if (isLoggedIn) {
       loginLogoutContainer.innerHTML = `
-        <a href="/logout" class="text-pink-600 text-xl font-bold hover:underline">Logout</a>
+        <a href="#" id="logout-button" class="text-pink-600 text-xl font-bold hover:underline">Logout</a>
       `;
     } else {
       loginLogoutContainer.innerHTML = `
         <a href="/login" class="mr-4 text-pink-600 text-xl font-bold hover:underline">Login</a>
         <a href="/signup" class="mr-4 text-pink-600 text-xl font-bold hover:underline">Sign Up</a>
       `;
+    }
+    
+    // handling logging out
+    if (isLoggedIn) {
+      logoutLink.addEventListener('click', async (event) => {
+        event.preventDefault();
+    
+        try {
+          const response = await fetch('/logout', {
+            method: 'GET',
+          });
+      
+          if (response.ok) {
+            // If logout is successful, redirect the user to the homepage or any other desired page
+            window.location.href = '/';
+          } else {
+            // Handle error if logout is not successful
+            alert('Logout failed. Please try again.');
+          }
+        } catch (err) {
+          console.error('Error:', err);
+          alert('An error occurred. Please try again later.');
+        }
+      });
     }
   };
   
