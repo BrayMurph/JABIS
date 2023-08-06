37cf6d805662ec08d2e7beda2059cffd39437959
Asiak <asiakemp24@gmail.com>
2023-08-06 16:32:21 -0400

    deleted code that was commented out

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,31 +1,4 @@
-// // logout.js
-// document.addEventListener("DOMContentLoaded", () => {
-//     const logoutButton = document.getElementById("logout-button");
-  
-//     // Add event listener to the logout button
-//     logoutButton.addEventListener("click", async (event) => {
-//       event.preventDefault();
-  
-//       try {
-//         // Send a request to the logout route on the server
-//         const response = await fetch("/logout", {
-//           method: "GET",
-//         });
-  
-//         // Check if the logout was successful
-//         if (response.ok) {
-//           // Redirect the user to the homepage or any other appropriate page
-//           window.location.replace("/");
-//         } else {
-//           console.error("Logout failed");
-//         }
-//       } catch (err) {
-//         console.error("Error during logout:", err);
-//       }
-//     });
-//   });
-  
-const logout = async () => {
+ const logout = async () => {
     const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
