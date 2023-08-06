59f1cbdd3cd9b3ebd99f62418c8a84f0c382756b
Asiak <asiakemp24@gmail.com>
2023-08-06 09:50:50 -0400

    added id in the handlebars

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,49 +1,58 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Getting references to our form and inputs
-    const signupForm = document.querySelector("form.signup");
-    const firstNameInput = document.querySelector("input#first_name");
-    const lastNameInput = document.querySelector("input#last_name");
-    const emailInput = document.querySelector("input#email");
-    const usernameInput = document.querySelector("input#username");
-    const passwordInput = document.querySelector("input#password");
-  
-    // When the form is submitted, we validate the inputs and call the signupUser function
-    signupForm.addEventListener("submit", async (event) => {
-      event.preventDefault();
-      const firstName = firstNameInput.value.trim();
-      const lastName = lastNameInput.value.trim();
-      const email = emailInput.value.trim();
-      const username = usernameInput.value.trim();
-      const password = passwordInput.value.trim();
-  
-      if (!firstName || !lastName || !email || !username || !password) {
-        return;
-      }
-  
-      try {
-        // If all fields are filled, we call the signupUser function and clear the form
-        await signupUser(firstName, lastName, email, username, password);
-        signupForm.reset();
-      } catch (err) {
-        console.log(err);
-      }
-    });
-  
-    // signupUser does a post to our "/signup" route and if successful, redirects us to the login page
-    async function signupUser(firstName, lastName, email, username, password) {
-      const response = await fetch("/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ firstName, lastName, email, username, password }),
-      });
-  
-      if (response.ok) {
-        window.location.replace("/login");
-      } else {
-        throw new Error("Signup failed");
-      }
+  // Setting elements to constants
+  const signupForm = document.querySelector("form.signup");
+  const firstNameInput = document.querySelector("input#first_name");
+  const lastNameInput = document.querySelector("input#last_name");
+  const emailInput = document.querySelector("input#email");
+  const usernameInput = document.querySelector("input#username");
+  const passwordInput = document.querySelector("input#password");
+
+  // Event listener for form submission
+  signupForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    // Get values from input fields
+    const first_name = firstNameInput.value.trim();
+    const last_name = lastNameInput.value.trim();
+    const email = emailInput.value.trim();
+    const username = usernameInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    // Check if any of the fields are empty
+    if (!first_name || !last_name || !email || !username || !password) {
+      return;
+    }
+
+    try {
+      // Send signup data to the API endpoint
+      await signupUser({ first_name, last_name, email, username, password });
+
+      // Clear the input fields after successful signup
+      firstNameInput.value = "";
+      lastNameInput.value = "";
+      emailInput.value = "";
+      usernameInput.value = "";
+      passwordInput.value = "";
+
+      // Redirect to the login page after successful signup
+      window.location.replace("/login");
+    } catch (err) {
+      console.log(err);
+      // Handle the error appropriately (e.g., show an error message)
     }
   });
-  
+
+  async function signupUser(userData) {
+    const response = await fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+
+    if (!response.ok) {
+      throw new Error("Signup failed");
+    }
+  }
+});
