d0779dc25fd6a3224d0bc3809f2cf4de82517d66
Asiak <asiakemp24@gmail.com>
2023-08-12 16:30:17 -0400

    login,signup, signout working and created user sessions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,24 +1,24 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-// // Route to check if the user is logged in
-// router.get('/checkLoggedIn', (req, res) => {
-//   if (req.user) {
-//     res.status(200).json({ isLoggedIn: true});
-//   } else {
-//     res.status(200).json({ isLoggedIn: false});
-//   }
-// });
+// Route to check if the user is logged in
+router.get('/checkLoggedIn', (req, res) => {
+  if (req.user) {
+    res.status(200).json({ isLoggedIn: true});
+  } else {
+    res.status(200).json({ isLoggedIn: false});
+  }
+});
 
-// router.post('/logout', (req, res) => {
-//   if (req.session.logged_in) {
-//     req.session.destroy(() => {
-//       res.status(204).end();
-//     });
-//   } else {
-//     res.status(404).end();
-//   }
-// });
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 
-// module.exports = router;
+module.exports = router;
